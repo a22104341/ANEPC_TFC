a30f907afb485d19529793e2cf9e6fb4e6cf6f99
a22104341 <diogo.m.rodrigues2003@googlemail.com>
2024-02-26 18:24:30 +0000

    IT LOADS STUFF NOW

--- src/main/frontend/src/components/DispositivoDropDown_TEST.js
+++ src/main/frontend/src/components/DispositivoDropDown_TEST.js
@@ -45,7 +45,73 @@ function DispositivoDropDown_TEST() {
     };
 
     return (
-        <div>JohnDoe</div>
+        <Form id="form_Page1" onSubmit={handleSubmit}>
+            <Form.Group controlId="formDispositivo">
+                <Form.Label>Select an option</Form.Label>
+                <Form.Control as="select" onChange={handleDropdownChange}>
+                    <option value="DIOPS">DIOPS</option>
+                    <option value="DECIR">DECIR</option>
+                    <option value="DIONRBQ">DIONRBQ</option>
+                    <option value="DIRACAERO">DIRACAERO</option>
+                    <option value="Especial">Especial</option>
+                </Form.Control>
+            </Form.Group>
+
+            <div id="eventType" className={selectedOption !== 'DECIR' ? 'd-block' : 'd-none'}>
+                <Form.Group controlId="formEventType">
+                    <Form.Label>Select an EventType</Form.Label>
+                    <Form.Control as="select" onChange={handleDropdownChange}>
+                        <option value="MeteorologiaAdversa">Meteorologia Adversa</option>
+                        <option value="Outro">Outro</option>
+                    </Form.Control>
+                </Form.Group>
+            </div>
+
+            <div id="other" className={selectedOption !== 'DECIR' || selectedOption !== 'MeteorologiaAdversa' ? 'd-block' : 'd-none'}>
+                <Form.Group controlId="otherTextField">
+                    <Form.Label>Other:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter your input here"
+                        value={otherInput} // Assuming you have a state for this
+                    />
+                </Form.Group>
+            </div>
+
+            <div id="fenomenoMeteorologico" className={selectedOption !== 'DECIR' || selectedOption !== 'Outro'? 'd-block' : 'd-none'}>
+                <Form.Group controlId="formConditions">
+                    <Form.Label>Select meteorological conditions</Form.Label>
+                    <Form.Control as="select" multiple onChange={handleConditionsChange}>
+                        <option value="Chuva">Chuva</option>
+                        <option value="Neve">Neve</option>
+                        <option value="Granizo">Granizo</option>
+                        <option value="Vento">Vento</option>
+                        <option value="Frio">Frio</option>
+                        <option value="Calor">Calor</option>
+                        <option value="Nevoeiro">Nevoeiro</option>
+                        <option value="Trovoada">Trovoada</option>
+                        <option value="Agitação Marítima">Agitação Marítima</option>
+                        <option value="Other">Outro</option>
+                    </Form.Control>
+                    {selectedConditions.includes('Other') && (
+                        <Form.Control
+                            type="text"
+                            placeholder="Outro"
+                            value={otherInput}
+                            onChange={handleOtherInputChange}
+                        />
+                    )}
+                </Form.Group>
+            </div>
+
+            <div id="evolucaoEAE" className={selectedOption !== 'Outro' ? 'd-block' : 'd-none'}>
+                {/* Content for DIRACAERO or Especial */}/*
+            </div>
+
+
+            <button type="submit">Submit</button>
+        </Form>
+
     );
 }
 
